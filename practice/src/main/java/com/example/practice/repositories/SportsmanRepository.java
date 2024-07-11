b6ae2b337c495ebc2845b0f63d5e33620f9cfcac
phounexxx <denissotovggg223@gmail.com>
2024-07-11 19:16:00 +0300

    fix overdue sportsman repository

--- practice/src/main/java/com/example/practice/repositories/SportsmanRepository.java
+++ practice/src/main/java/com/example/practice/repositories/SportsmanRepository.java
@@ -1,8 +1,11 @@
 package com.example.practice.repositories;
 
 import com.example.practice.entities.Sportsman;
+import jakarta.persistence.TemporalType;
+import org.hibernate.type.descriptor.DateTimeUtils;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Temporal;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +16,9 @@ import java.util.List;
 public interface SportsmanRepository extends NameRepository<Sportsman> {
 
     //Search for the sportsman who has overdue category
-    @Query(value = "select s from Sportsman s where s.reachDate + 730 > LOCAL_DATETIME")
-    List<Sportsman> findAllByReachDateOrderBySurname();
+    @Query(value = "select s from Sportsman s where  s.reachDate > :currentDateMinusTwoYears ")
+    List<Sportsman> findAllByReachDateOrderBySurname(@Param(value = "currentDateMinusTwoYears")
+                                                     Date date);
 
     //Search for the sportsman with entry birthday and gender to make a swimming queue
     @Query(value = "select s from Sportsman s "+
@@ -23,5 +27,5 @@ public interface SportsmanRepository extends NameRepository<Sportsman> {
             @Param(value = "birthDate") Sportsman birthDate,
             @Param(value = "gender") Sportsman gender);
 
-    List<Sportsman> findAllByReachDateOrderBySurname(Date date);
+    List<Sportsman> findAllByReachDate(Date date);
 }
